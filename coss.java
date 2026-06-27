852e43e937f32b0418c4f1aa203a582a12939326
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-06-27 21:12:36 +0100

    Delete coss.java

--- coss.java
+++
@@ -1,9 +0,0 @@
-import java.util.InputMismatchException;
-import java.util.Scanner;
-
-public class coss {
-    public static void main(String[] args) {
-        double a=Math.cos(1.00f);
-        System.out.println(a);
-    }
-}
